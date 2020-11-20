76a7e6936f38cf0759b65a84ae65ce30320e123a
Jared Hill <jaredbhill@gmail.com>
2020-11-20 12:14:52 -0600

    adding version checking to publish workflow

--- .storybook/StoryWrapper.tsx
+++ .storybook/StoryWrapper.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Base } from '../src';
 
-// import '../src/styles/index.scss';
 import './storybook-preview.scss';
 
 export const StoryWrapper = ({ children }) => <Base className="storybook-wrapper">{children}</Base>;
