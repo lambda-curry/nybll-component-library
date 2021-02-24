1821328afdd698535336ec49e62d6a8ed5e4d671
Carlo <carlo.piantini@gmail.com>
2021-02-24 11:38:39 -0500

    UpdateMenuStyles Adjusted font-size and line-height styles for tabs; added min-height of 450px to internal tab panels.

--- src/components/Menu/MenuContentPanel.tsx
+++ src/components/Menu/MenuContentPanel.tsx
@@ -26,7 +26,7 @@ export const MenuContentPanel: React.FC<MenuContentPanelProps> = ({ className, i
 
           <div className="nybll-grid">
             <div className="nybll-grid__col-xs-12">
-              <p className="">We're sorry, but that menu is currently unavailable. Please check again later!</p>
+              <p className="margin-0">We're sorry, but that menu is currently unavailable. Please check again later!</p>
             </div>
           </div>
         </div>
